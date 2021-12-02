46382b067dbc9879267443bde0fbc1b6d3569f18
raven <zlat.dominic.francisc@gmail.com>
2021-12-02 02:57:40 +0200

    Code Restructuring

--- server.js
+++ server.js
@@ -4,20 +4,28 @@ const app = express();
 const server = http.Server(app);
 const socketio = require('socket.io');
 const path = require('path');
-const { isContext } = require('vm');
 const io_i= socketio(server);
 const forever = require('forever');
 const five = require('johnny-five');
-const acnt_link = '1f81601c-f713-4313-8fdb-f0c4c531c806';
+const acnt_link = '600e8c51-74e3-4b34-ac79-a704c5307848';//1f81601x-f713-4313-8fdb-f0c4c531c806 [permanent account ref req for vulture ownership and access control]
 let Servo_, Board_, Proximity_, Accelerometer_, IMU_, GPS_, board_, Pin_;
 
+///--Global general purpose functions [gg]--///
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+////--Process Up Insurance--////
 
 process.on('exit', () => {
     forever.restart();
 });
+
+//enables or disables hardware link req for dev purposes [dev]
 var hardware_enabled = false;
 //run forever for remote reboot capabilities
 
+//enables or disables telemetry broadcast of specific sensors dev purposes [dev]
 var sec_acc_acivs = true;
 var imu_acivs = true;
 var s1_acivs = false;
@@ -28,6 +36,7 @@ var s5_acivs = false;
 var gps_acivs = false;
 var alt_acivs = true;
 
+//array telemetry relay status to Advanced_Telemetry F/E
 let sensor_array_active_status = [sec_acc_acivs, imu_acivs, s1_acivs, s2_acivs, s3_acivs, s4_acivs, s5_acivs, gps_acivs, hardware_enabled, alt_acivs];
 if(hardware_enabled == false){
   sensor_array_active_status = [false, false, false, false, false, false, false, false];
@@ -41,22 +50,22 @@ if(hardware_enabled == true){
   Accelerometer_ = Accelerometer;
   IMU_ = IMU;
   GPS_ = GPS;
-  const board = new Board({port: "COM5"});
+  const board = new Board({port: "/dev/ttyUSB0"});
   board_ = board;
   Pin_ = Pin;
 }
 
 
 var io = require('socket.io-client');
-const { SCHED_NONE } = require('cluster');
-const nodemon = require('nodemon');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //web_app: https://boiling-citadel-40139.herokuapp.com/
 //local: http://localhost:3300/
 //H2 local: https://localhost:3900/
 
-var socket = io.connect("http://localhost:3300/", {reconnection: true});
+var socket = io.connect("ws://boiling-citadel-40139.herokuapp.com/", {reconnection: true});
+
+////--Hardware Status Overview vars--////
 var imu_connected_status;
 var imu_connection_status_bin = 0;
 var axdl_connection_status_bin = 0;
@@ -79,6 +88,8 @@ var gps_nav_connection_status;
 var barometer_connection_status;
 
 var RTH = 0;
+
+//experimental hard reset for specific sensors
 var imu_pin_;
 var axdl_pin_;
 
@@ -86,27 +97,27 @@ socket.on("connect_error", (err) => {
   console.log(`connect_error due to ${err}`);
 });
 
-socket.on('connect', () => {
-  socket.on('m1_manual_thrust_lvl_rebound', (rcvd_m1_thrust_lvl) => {
-    if(rcvd_m1_thrust_lvl >= 0 && rcvd_m1_thrust_lvl <= 100){
-      //m1.throttle(rcvd_m1_thrust_lvl);
-      console.log(`M1 | --${rcvd_m1_thrust_lvl} | ${Date.now()}`);
-    }
-  });
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-  
+
+
+
+////--this ⇄ Server | Telemetry Relay to: Advanced_Telemetry F/E, Command--////
+socket.on('connect', (s) => {
+
+
+  //artificial sonar array dev purposes [dev]
   setInterval(() => {
-    socket.emit('sonar_1_rebound', getRandomInt(400));
+    socket.emit('sonar_1_rebound', {payload: getRandomInt(1000), id: acnt_link});
     socket.emit('sonar_2_rebound', getRandomInt(400));
     socket.emit('sonar_3_rebound', getRandomInt(400));
     socket.emit('sonar_4_rebound', getRandomInt(400));
-    socket.emit('sonar_5_rebound', getRandomInt(400));
+    socket.emit('sonar_5_rebound', getRandomInt(1));
   }, 1000);
   
+  ///--this ⇄ Server | ID handshake for req for client link--///
   socket.emit('vulture_online_signal', "uplink established");
   socket.emit('vulture_ownr_link', acnt_link);
+  
+  ///--this ⇄ Server | client CLI parser and execution--///
   socket.on('parsed_cli_cmd', cmd_payload => {
     var scmd_payload = cmd_payload.split(":");
     if(scmd_payload[0].toUpperCase() == "RTH"){
@@ -136,66 +147,73 @@ socket.on('connect', () => {
       }
     }
  });
+  socket.on('be_rth_status_rqst', () => {
+    socket.emit('rth_status_rqst_payload', RTH);
+  });
 
+  ///--this ⇄ Server | special client CMDS--///
   socket.on('vulture_server_reboot_server_relay', () => {
     console.log("server reboot command received");
     process.exit();
   });
-  socket.on('omega_reboot_signal_relay', () => {
+  socket.on('omega_reboot_signal_relay', () => {// Omega Hardware Interface Board Restart
     console.log("server reboot command received");
     process.exit();
-  })
-    console.log("uplink established");
+  });
+
+
+  console.log("uplink established");// client link ack
     if(hardware_enabled){
-board_.on("ready", () => {
-  const m1 = new five.ESC({pin: 9, pwmRange:[1000, 2000]});//pwmRange:[1290, 2000]
-  console.log('working_bitch');
-  socket.on('m1_manual_thrust_lvl_rebound', (rcvd_m1_thrust_lvl) => {
-    if(rcvd_m1_thrust_lvl >= 0 && rcvd_m1_thrust_lvl <= 100){
-      m1.throttle(rcvd_m1_thrust_lvl);
-      console.log(`M1 | --${rcvd_m1_thrust_lvl} | ${Date.now()}`);
-    }
-  });
-  socket.on('m2_manual_thrust_lvl_rebound', (rcvd_m2_thrust_lvl) => {
-    if(rcvd_m2_thrust_lvl >= 0 && rcvd_m2_thrust_lvl <= 100){
-      console.log(`M2 | --${rcvd_m2_thrust_lvl} | ${Date.now()}`);
-    }
-  });
-  socket.on('m3_manual_thrust_lvl_rebound', (rcvd_m3_thrust_lvl) => {
-    if(rcvd_m3_thrust_lvl >= 0 && rcvd_m3_thrust_lvl <= 100){
-      console.log(`M3 | --${rcvd_m3_thrust_lvl} | ${Date.now()}`);
-    }
-  });
-  socket.on('m4_manual_thrust_lvl_rebound', (rcvd_m4_thrust_lvl) => {
-    if(rcvd_m4_thrust_lvl >= 0 && rcvd_m4_thrust_lvl <= 100){
-      console.log(`M4 | --${rcvd_m4_thrust_lvl} | ${Date.now()}`);
-    }
-  });
+      board_.on("ready", () => {//hardware interface board ini
 
-  const imu_pin = new five.Pin({
-    pin: 2
-  });
-  const axdl_pin = new five.Pin({
-    pin: 4
-  });
+  ////---Propulsion---////[prop]
 
-      axdl_pin.high();
-      imu_pin.low();
-     if(sensor_array_active_status[0] == true){
-      const imu = new IMU_({
-        controller: "MPU6050",
-        address: 0x68
+      ///--Propulsion Manual Testing Controller--///
+      const m1 = new five.ESC(11);//pwmRange:[1290, 2000]
+      socket.on('m1_manual_thrust_lvl_rebound', (rcvd_m1_thrust_lvl) => {
+        if(rcvd_m1_thrust_lvl >= 0 && rcvd_m1_thrust_lvl <= 100){
+          m1.throttle(rcvd_m1_thrust_lvl);
+          console.log(`M1 | --${rcvd_m1_thrust_lvl} | ${Date.now()}`);
+        }
       });
-      imu.on('change', () =>{
-        const imu_data_pkg = [imu.thermometer.celsius - 18, imu.accelerometer.x, imu.accelerometer.y, imu.accelerometer.z, imu.accelerometer.pitch, imu.accelerometer.roll, imu.accelerometer.acceleration, imu.accelerometer.inclination, imu.accelerometer.orientation,
-        imu.gyro.x, imu.gyro.y, imu.gyro.z, imu.gyro.pitch, imu.gyro.roll, imu.gyro.yaw, imu.gyro.rate, imu.gyro.isCalibrated];
-        socket.emit('imu_data_pkg_broadcast', imu_data_pkg);
-        socket.emit('gamma_board_hs');
-        imu_connection_status_bin = Date.now();
-       // console.log(detect_hardware_connection);
+
+      socket.on('m2_manual_thrust_lvl_rebound', (rcvd_m2_thrust_lvl) => {
+        if(rcvd_m2_thrust_lvl >= 0 && rcvd_m2_thrust_lvl <= 100){
+          console.log(`M2 | --${rcvd_m2_thrust_lvl} | ${Date.now()}`);
+        }
+      });
+
+      socket.on('m3_manual_thrust_lvl_rebound', (rcvd_m3_thrust_lvl) => {
+        if(rcvd_m3_thrust_lvl >= 0 && rcvd_m3_thrust_lvl <= 100){
+          console.log(`M3 | --${rcvd_m3_thrust_lvl} | ${Date.now()}`);
+        }
+      });
+
+      socket.on('m4_manual_thrust_lvl_rebound', (rcvd_m4_thrust_lvl) => {
+        if(rcvd_m4_thrust_lvl >= 0 && rcvd_m4_thrust_lvl <= 100){
+          console.log(`M4 | --${rcvd_m4_thrust_lvl} | ${Date.now()}`);
+        }
+      });
+
+
+  ////---Dynamics---////[dyn]
+
+      ///--IMU Telemetry Broadcaster & Status Watcher | HID [02C] | [temperture(C), x, y, z, pitch, roll, accel, inclination, orientation, Gyro x, Gyro y, Gyro z, Gyro pitch, Gyro roll, Gyro yaw, Gyro rate, Gyro isCalibrated]--///
+      if(sensor_array_active_status[0] == true){
+        const imu = new IMU_({
+          controller: "MPU6050",
+          address: 0x68
         });
-     }
-     
+        imu.on('change', () =>{
+          const imu_data_pkg = [imu.thermometer.celsius - 18, imu.accelerometer.x, imu.accelerometer.y, imu.accelerometer.z, imu.accelerometer.pitch, imu.accelerometer.roll, imu.accelerometer.acceleration, imu.accelerometer.inclination, imu.accelerometer.orientation,
+          imu.gyro.x, imu.gyro.y, imu.gyro.z, imu.gyro.pitch, imu.gyro.roll, imu.gyro.yaw, imu.gyro.rate, imu.gyro.isCalibrated];
+          socket.emit('imu_data_pkg_broadcast', imu_data_pkg);
+          socket.emit('gamma_board_hs');
+          imu_connection_status_bin = Date.now();
+        });
+      }
+
+      ///--Acc Telemetry Broadcaster & Status Watcher | HID [02E] | [acceleration, inclination, orientation, pitch, roll, x, y, z]--///     
       if(sensor_array_active_status[1] == true){
         const accelerometer = new Accelerometer_({
           controller: "ADXL345",
@@ -204,13 +222,16 @@ board_.on("ready", () => {
         accelerometer.on('change', () =>{
           const {acceleration, inclination, orientation, pitch, roll, x, y, z} = accelerometer;
           const data_pkg = [acceleration, inclination, orientation, pitch, roll, x, y, z];
-          //socket.emit('gyro_data', pitch * (-1));
           socket.emit('gyro_data_pkg', data_pkg);
           socket.emit('gamma_board_hs');
           axdl_connection_status_bin = Date.now();
-          //console.log("Pitch: " + data_pkg[3] + " | Roll: " + data_pkg[4]);
         })
       }
+
+
+  ////---Nav---////[nav]
+
+      ///--Barometer Telemetry Broadcaster & Status Watcher | HID [02I] | [pressure(Pa), temperture(C), altitude(m)]--///     
       if(sensor_array_active_status[9] == true){
         var barometer = new five.Multi({
           controller: "BMP280",
@@ -222,13 +243,10 @@ board_.on("ready", () => {
           var alt_acx = (((Math.pow((101325 / (this.barometer.pressure * 1000)), (1 / 5.257)) - 1) * this.thermometer.kelvin) / 0.0065);
           socket.emit('barometer_pkg', [this.barometer.pressure, barometer.thermometer.celsius, alt_acx]);
           barometer_connection_status_bin = Date.now();
-          //console.log(this.barometer.pressure * 1000);
-          //console.log(alt_acx);
-          //console.log(altimeter.thermometer.celsius);
         });
       }
 
-
+      ///--GPS Telemetry Broadcaster & Status Watcher | HID [02F] | [sat_ops][Sat Ops] [gps_position_pkg][longitude, latitude, altitude] [gps_nav_pkg][heading, speed]--///     
       if(sensor_array_active_status[7] == true){
         var gps = new GPS_({
           pins: {
@@ -237,7 +255,7 @@ board_.on("ready", () => {
           }
         });
       
-         gps.on("change", position => {
+        gps.on("change", position => {
           const {altitude, latitude, longitude} = position;
           var gps_position_pkg = [longitude, latitude, altitude];
           socket.emit('gps_position_pkg', gps_position_pkg);
@@ -251,14 +269,12 @@ board_.on("ready", () => {
           
         });
         gps.on('message', (nmea_stnc) => {
-          console.log(`${nmea_stnc} lmao`);
+          console.log(`${nmea_stnc} lmao`);//[dev]
         });
         gps.on("operations", _sat_ops =>{
            socket.emit('sat_ops', _sat_ops);
            socket.emit('gamma_board_hs');
-          // console.log(" sat_ops    : ", sat_ops);
         });
-        // If speed, course change log it
         gps.on("navigation", velocity => {
           const {course, speed} = velocity;
           var gps_nav_pkg = [course, speed];
@@ -272,9 +288,11 @@ board_.on("ready", () => {
           
         });
       }
-     // });
+      
+  ////---Sonar Array---////[sar]
 
-     if(sensor_array_active_status[2] == true){
+      ///--FWD Sonar Telemetry Broadcaster & Status Watcher | HID [02D-0] | [Front Sonar distance(cm)]--///     
+      if(sensor_array_active_status[2] == true){
         const soanr_1 = new Proximity_({
           controller: "HCSR04",
           pin: 7
@@ -286,11 +304,13 @@ board_.on("ready", () => {
           sonar_1_connection_status_bin = Date.now();
         });
       }
-     
+
+
+      ///--LFT Sonar Telemetry Broadcaster & Status Watcher | HID [02D-1] | [Left-side Sonar distance(cm)]--///     
       if(sensor_array_active_status[3] == true){
         const soanr_2 = new Proximity_({
           controller: "HCSR04",
-          pin: 11
+          pin: 13
         });
         soanr_2.on("change", () => {
           const {centimeters} = soanr_2;
@@ -300,7 +320,9 @@ board_.on("ready", () => {
         });
        }
 
-       if(sensor_array_active_status[4] == true){
+
+      ///--BWD Sonar Telemetry Broadcaster & Status Watcher | HID [02D-2] | [Rear Sonar distance(cm)]--///
+      if(sensor_array_active_status[4] == true){
         const soanr_3 = new Proximity_({
           controller: "HCSR04",
           pin: 5
@@ -311,9 +333,11 @@ board_.on("ready", () => {
           socket.emit('gamma_board_hs');
           sonar_3_connection_status_bin = Date.now();
         });
-       }
+      }
 
-       if(sensor_array_active_status[5] == true){
+
+      ///--RGT Sonar Telemetry Broadcaster & Status Watcher | HID [02D-3] | [Right-side Sonar distance(cm)]--///
+      if(sensor_array_active_status[5] == true){
         const soanr_4 = new Proximity_({
           controller: "HCSR04",
           pin: 6
@@ -324,9 +348,11 @@ board_.on("ready", () => {
           socket.emit('gamma_board_hs');
           sonar_4_connection_status_bin = Date.now();
         });
-       }
+      }
 
-       if(sensor_array_active_status[6] == true){
+
+      ///--GND Sonar Telemetry Broadcaster & Status Watcher | HID [02D-4] | [Ground Sonar distance(cm)]--///
+      if(sensor_array_active_status[6] == true){
         const soanr_5 = new Proximity_({
           controller: "HCSR04",
           pin: 9
@@ -337,17 +363,42 @@ board_.on("ready", () => {
           socket.emit('gamma_board_hs');
           sonar_5_connection_status_bin = Date.now();
         });
-       }
-       socket.on('imu_restart_signal_sr', () => {
+      }
+
+      
+      //--experimental sensor hard reset via solid state relays--//
+      const relay_ch0 = new five.Pin(8);
+      const relay_ch1 = new five.Pin(10);
+      var relay_ch0_state = false;
+      var relay_ch1_state = true;
+      socket.on('imu_restart_signal_sr', () => {
          //imu_pin_ = imu_pin;
-        imu_pin.high();
-        setTimeout(() => {
-          imu_pin.low();
-        }, 100);
+        // imu_pin.high();
+        // setTimeout(() => {
+        //   imu_pin.low();
+        // }, 100);
+        if(!relay_ch0_state){
+          relay_ch0.high();
+          relay_ch0_state = true;
+        }
+        else{
+          relay_ch0.low();
+          relay_ch0_state = false;
+        }
+        if(relay_ch1_state){
+          relay_ch1.low();
+          relay_ch1_state = false;
+        }
+        else{
+          relay_ch1.high();
+          relay_ch1_state = true;
+        }
        });
        
-       const b_led = new five.Led(7);
-       const r_led = new five.Led(6);
+      
+      //--Board controller link status indicator--//
+      const b_led = new five.Led(7);
+      //const r_led = new five.Led(6);
        setInterval(() => {
          b_led.on();
          setTimeout(() => {
@@ -362,17 +413,19 @@ board_.on("ready", () => {
      
        }, 1200);
        
-       setInterval(() => {
-         r_led.fadeIn();
-         setTimeout(() => {
-           r_led.fadeOut();
-         }, 500)
-       }, 1500)
+      //  setInterval(() => {
+      //    r_led.fadeIn();
+      //    setTimeout(() => {
+      //      r_led.fadeOut();
+      //    }, 500);
+      //  }, 1500);
 
    });
-   }
+}
+
+////---Hardware Status Compute and broadcast---////[sar]
     setInterval(() =>{
-     
+      //Dynamics//
         if(Math.abs(imu_connection_status_bin - Date.now()) > 200){
           imu_connected_status = false;
         }
@@ -385,6 +438,8 @@ board_.on("ready", () => {
         else{
           axdl_connection_status = true;
         }
+
+      //Sonar Array//
         if(Math.abs(sonar_1_connection_status_bin - Date.now()) > 600){
           sonar_1_connection_status = false;
         }
@@ -415,6 +470,8 @@ board_.on("ready", () => {
         else{
           sonar_5_connection_status = true;
         }
+
+      //Nav//
         if(Math.abs(gps_position_connection_status_bin - Date.now()) > 1200){
           gps_position_connection_status = false;
         }
@@ -433,35 +490,34 @@ board_.on("ready", () => {
         else{
           barometer_connection_status = true;
         }
+
+      //Broadcast//
         var sensor_array_hardware_cs = [imu_connected_status, axdl_connection_status, sonar_1_connection_status, sonar_2_connection_status, sonar_3_connection_status, sonar_4_connection_status, sonar_5_connection_status, gps_position_connection_status, barometer_connection_status];
         socket.emit('sensor_array_hardware_cs', sensor_array_hardware_cs);
-        
+      
         socket.emit('sensor_array_es', sensor_array_active_status);
     }, 250)
 
+////--Ping Emitters--////
+
     setInterval(() =>{
       socket.emit('local_server_ping_emitter', Date.now());
-   }, 50)
+    }, 50);
   
     socket.on('local_relay_ping', () =>{
       socket.emit('local_relay_ping_back');
-    })
+    });
+
+////--User Input--////
     socket.on('input_pkg_local_relay_emitter', joystick_input_pkg =>{
       io_i.emit('input_pkg_local_relay', joystick_input_pkg);
       socket.emit('input_pkg_local_rebound', joystick_input_pkg);
     });
-    socket.on('be_rth_status_rqst', () => {
-      socket.emit('rth_status_rqst_payload', RTH);
-    });
+
+})
 
 
-    socket.on('client_event', function(data){
-       // console.log(data);
-    })
-  })
-
-
-server.listen(3400);
+server.listen(3410);
 
 
 //cuz vendor fuck up
